84fdfe339d647900decc997205c4899e2c708a37
William Vannuffelen <williamvannuffelen@gmail.com>
2020-03-11 11:14:46 +0100

    Refactor to functions

--- win_scripts/Get-AnsiblePrereqInfo.ps1
+++ win_scripts/Get-AnsiblePrereqInfo.ps1
@@ -1,4 +1,6 @@
-$psVersionInfo = $psVersionTable |
+# check PS version
+function Get-PSVersionInfo{
+    $psVersionInfo = $psVersionTable |
     Select-Object   @{Name="ps_version_simple";     Expression={"$($_.psVersion.major).$($_.psVersion.minor)"}},
                     @{Name="ps_version_major";      Expression={$_.psVersion.major}},
                     @{Name="ps_compatible";         Expression={
@@ -7,26 +9,36 @@ $psVersionInfo = $psVersionTable |
                             default {$false}
                         }
                     }}
+    return $psVersionInfo
+}
 
-$dotNetRegPath = "HKLM:\Software\Microsoft\Net Framework Setup\NDP\v4\Full"
-$dotNetMinRelease = 379893 # 4.5.2
-if(-not (Test-Path -Path $dotNetRegPath)){
-    $dotNetVersionInfo = [PSCustomObject]@{
-        "dotnet_version" = "n/a"
-        "dotnet_release" = "n/a"
-        "dotnet_compatible" = $false
+
+# check .NET version
+function Get-DotNetVersionInfo{
+    $dotNetRegPath = "HKLM:\Software\Microsoft\Net Framework Setup\NDP\v4\Full"
+    $dotNetMinRelease = 379893 # 4.5.2
+    if(-not (Test-Path -Path $dotNetRegPath)){
+        $dotNetVersionInfo = [PSCustomObject]@{
+            "dotnet_version" = "n/a"
+            "dotnet_release" = "n/a"
+            "dotnet_compatible" = $false
+        }
     }
-}
-else{
-    $dotNetVersionInfo = Get-ItemProperty -Path $dotNetRegPath | 
-        Select-Object   @{Name="dotnet_version";    Expression={$_.version}},
-                        @{Name="dotnet_release";    Expression={$_.release}},
-                        @{Name="dotnet_compatible"; Expression={
-                            switch ($_.release){
-                                {$_ -ge $dotNetMinRelease} {$true}
-                                default {$false}
-                            }
-                        }}
+    else{
+        $dotNetVersionInfo = Get-ItemProperty -Path $dotNetRegPath | 
+            Select-Object   @{Name="dotnet_version";    Expression={$_.version}},
+                            @{Name="dotnet_release";    Expression={$_.release}},
+                            @{Name="dotnet_compatible"; Expression={
+                                switch ($_.release){
+                                    {$_ -ge $dotNetMinRelease} {$true}
+                                    default {$false}
+                                }
+                            }}
+    }
+    return $dotNetVersionInfo
 }
 
-write-host "no"
+# if PS 3.0, check WinRM memory hotfix
+function Get-WinRMHotfixStatus($psVersionInfo){
+    
+}
