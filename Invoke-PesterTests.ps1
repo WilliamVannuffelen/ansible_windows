1139bd29dcdd67c2edff30f18763014b6a25a41b
William Vannuffelen <williamvannuffelen@gmail.com>
2020-03-16 12:02:41 +0100

    Install more recent version of Pester on build agent to fix compatibility issues

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -1,8 +1,6 @@
-    "current location: $(Get-Location)"
-    "script root: $PSScriptRoot"
-    "retrieve available modules"
-    $modules = Get-Module -list
-    if($modules.Name -notcontains 'pester'){
-        Install-Module -Name Pester -Force -SkipPublisherCheck
-    }
-    Invoke-Pester -Script "./win_scripts/*.Tests.ps1" -OutputFile "./pester_test_results.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./win_scripts/*.ps1"
+"current location: $(Get-Location)"
+"script root: $PSScriptRoot"
+
+Install-Module -Name Pester -RequiredVersion "4.10.1" -SkipPublisherCheck
+    
+Invoke-Pester -Script "./win_scripts/*.Tests.ps1" -OutputFile "./pester_test_results.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./win_scripts/*.ps1"
