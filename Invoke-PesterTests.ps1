afc8d90b678974221cc1ed632d060a3f4d732593
William Vannuffelen <williamvannuffelen@gmail.com>
2020-03-16 12:06:52 +0100

    Add confirm flag to Pester installation

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -1,6 +1,6 @@
 "current location: $(Get-Location)"
 "script root: $PSScriptRoot"
 
-Install-Module -Name Pester -RequiredVersion "4.10.1" -SkipPublisherCheck
+Install-Module -Name Pester -RequiredVersion "4.10.1" -SkipPublisherCheck -Confirm:$false
     
 Invoke-Pester -Script "./win_scripts/*.Tests.ps1" -OutputFile "./pester_test_results.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./win_scripts/*.ps1"
