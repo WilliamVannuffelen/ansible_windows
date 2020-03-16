71d3faa6aad4bfa1626864de16289aadb8078e39
William Vannuffelen <williamvannuffelen@gmail.com>
2020-03-16 16:07:15 +0100

    Add comments

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -1,6 +1,5 @@
-"current location: $(Get-Location)"
-"script root: $PSScriptRoot"
-
+Write-Output "Installing Pester v4.10.1 on build agent"
 Install-Module -Name Pester -RequiredVersion "4.10.1" -SkipPublisherCheck -Force -Confirm:$false
-    
+
+Write-Output "Running Pester tests"
 Invoke-Pester -Script "./win_scripts/*.Tests.ps1" -OutputFile "./pester_test_results.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./win_scripts/*.ps1"
