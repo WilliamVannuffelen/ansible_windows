041be686436a3b39bf10771c5426d81a20abde41
William Vannuffelen <williamvannuffelen@gmail.com>
2020-03-16 17:52:17 +0100

    Add code coverage export and publish

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -2,4 +2,13 @@ Write-Output "Installing Pester v4.10.1 on build agent"
 Install-Module -Name Pester -RequiredVersion "4.10.1" -SkipPublisherCheck -Force -Confirm:$false
 
 Write-Output "Running Pester tests"
-Invoke-Pester -Script "./win_scripts/*.Tests.ps1" -OutputFile "./pester_test_results.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./win_scripts/*.ps1"
+
+$params = @{
+    script = "./win_scripts/*.Tests.ps1"
+    outputFile = "./pester_test_results.xml"
+    outputFormat = "NUnitXML"
+    codeCoverage = "./win_scripts/*.ps1"
+    codeCoverageOutputFile = "./pester_coverage_results.xml"
+    codeCoverageOutputFileFormat = "JaCoCo"
+}
+Invoke-Pester @params 
